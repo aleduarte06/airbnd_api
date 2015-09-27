bbc941cac13927a9e241045a5394572065f3f33d
Luciano Duarte <luciano0509@gmail.com>
2015-09-27 18:42:18 -0300

    User: - se agrego al modelo datos de facebook

--- models/users.js
+++ models/users.js
@@ -11,23 +11,31 @@ var userSchema = new Schema({
     fullname: String,
     active: Boolean,
     reservations: [Schema.Types.ObjectId],
-    apartments:[Schema.Types.ObjectId]
+    apartments:[Schema.Types.ObjectId],
+    social: {
+        facebook:{
+            id: String,
+            token: String
+        }
+    }
+
 });
 
 /* Generate Password */
 userSchema.pre('save', function(next){
     var user = this;
     if(!user.isModified('password'))  return next();
-
-    bcrypt.genSalt(8, function(err, salt){
-        if(err) return next(err);
-        console.log('a');
-        bcrypt.hash(user.password, salt, function(err, hash){
-            if (err) return next(err);
-            user.password = hash;
-            next()
-        })
-    });
+    if (user.password) {
+        bcrypt.genSalt(8, function(err, salt){
+            if(err) return next(err);
+            console.log('a');
+            bcrypt.hash(user.password, salt, function(err, hash){
+                if (err) return next(err);
+                user.password = hash;
+                next()
+            });
+        });
+    }
 });
 
 /* Verify Password */

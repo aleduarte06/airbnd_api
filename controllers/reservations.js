047f7639b6ce5b19eeb664f2b1bdeee99eaa6f17
Luciano Duarte <luciano0509@gmail.com>
2015-09-28 15:11:50 -0300

    Reservation: - al crear una nueva, ya se le asigna al usuario la reserva

--- controllers/reservations.js
+++ controllers/reservations.js
@@ -3,54 +3,85 @@
  */
 
 "use strict";
+var mongoose = require('mongoose'),
+    modelReservation = mongoose.model('Reservation'),
+    modelUser = mongoose.model('User');
 
-var modelReservation = require('mongoose').model('Reservation');
+var messageNotExist = 'Reservation does not exist';
 
-exports.getAll = function(req, res){
+exports.getAll = function (req, res) {
     modelReservation.find()
-        .then(function(reservations){
+        .then(function (reservations) {
             res.status(200).json(reservations);
         })
-        .catch(function(err) {
+        .catch(function (err) {
             res.status(500).json({message: err.message});
         })
 };
 
-exports.getOne = function(req, res) {
+exports.getOne = function (req, res) {
     modelReservation.findOne({"_id": req.params.id})
-        .then(function(err, reservation){
-            if(reservation)
+        .then(function (reservation) {
+            if (reservation)
                 res.status(200).json(reservation);
             else
-                res.status(404).json({message: 'Reservation does not exist'});
+                res.status(404).json({message: messageNotExist})
         })
-        .catch(function(err) {
-            res.status(500).json({message: err.message});
+        .catch(function (err) {
+            res.status(500).json({message: err.message})
         })
 };
 
-exports.create = function(req, res){
-    modelReservation.create(req.body).then(function(reservation){
-        res.status(201).json({message: 'New reservation', data: reservation});
-    },function(err){
+exports.create = function (req, res) {
+    modelReservation.create({
+        userID: req.user,
+        apartmentID: req.body.apartmentID,
+        startDate: req.body.startDate,
+        endDate: req.body.endDate
+    }).then(function (reservation) {
+        modelUser.findOne({"_id": req.user})
+            .then(function (user) {
+                user.reservations.push(reservation._id);
+                user.save();
+                res.status(201).json({message: 'New reservation', data: reservation});
+            })
+            .catch(function (err) {
+                res.status(500).json({message: err.message})
+            });
+    }, function (err) {
         res.status(500).json({message: err.message});
     })
 };
 
-exports.update = function(req, res){
+exports.update = function (req, res) {
     modelReservation.findOneAndUpdate({"_id": req.params.id}, req.body)
-        .then(function(reservation) {
-            res.status(200).json({message: 'Reservation has been updated'});
+        .then(function (reservation) {
+            if (reservation)
+                res.status(200).json({message: 'Reservation has been updated'});
+            else
+                res.status(404).json({message: messageNotExist})
         })
-        .catch(function(err){
+        .catch(function (err) {
             res.status(500).json({message: err.message});
         })
 };
 
-exports.delete = function(req, res) {
+exports.delete = function (req, res) {
     modelReservation.findOneAndRemove({"_id": req.params.id})
         .then(function(reservation) {
-            res.status(200).json({message: 'User deleted', data: reservation});
+            if (reservation) {
+                modelUser.findOne({"_id": req.user})
+                    .then(function (user) {
+                        req.user.reservations.splice(user.reservations.indexOf(reservation._id), 1);
+                        req.user.save();
+                        res.status(200).json({message: 'Reservation deleted', data: reservation});
+                    })
+                    .catch(function (err) {
+                        res.status(500).json({message: err.message})
+                    })
+            } else {
+                res.status(404).json({message: messageNotExist})
+            }
         })
         .catch(function(err){
             res.status(500).json({message: err.message});

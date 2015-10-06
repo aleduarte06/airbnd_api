8505c2f47bc7794aa594c9ef9be4d16bda5e6e62
aleduarte <aleduarte06@gmail.com>
2015-10-06 12:56:27 -0300

    saque nodemon y deje node start

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use(multer({ dest: './uploads/'}));
+//app.use(multer({ dest: './uploads/'}));
 
 /* Database */
 mongoose.connect(config.mongo_url, function (err) {
